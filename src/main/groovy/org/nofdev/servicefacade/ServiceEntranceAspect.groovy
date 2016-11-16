d42467c7d35c89fbc3c5664388725e5117b6ffc7
tfgzs <867805932@qq.com>
2016-11-16 15:18:10 +0800

    nonuse MDC

--- src/main/groovy/org/nofdev/servicefacade/ServiceEntranceAspect.groovy
+++ src/main/groovy/org/nofdev/servicefacade/ServiceEntranceAspect.groovy
@@ -6,10 +6,10 @@ import groovy.util.logging.Slf4j
 import org.aspectj.lang.JoinPoint
 import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.*
-import org.slf4j.MDC
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
+
 /**
  * Created by Qiang on 5/18/16.
  */
@@ -23,12 +23,12 @@ class ServiceEntranceAspect {
     private ObjectMapper objectMapper
 
     @Pointcut("execution(public * org.nofdev.servicefacade.*Controller.json(..))")
-    void entrancePointcut(){
+    void entrancePointcut() {
 
     }
 
     @Around("entrancePointcut()")
-    Object performance(ProceedingJoinPoint joinPoint){
+    Object performance(ProceedingJoinPoint joinPoint) {
         def args = joinPoint.args
         long startTime = System.currentTimeMillis()
         def object = joinPoint.proceed()
@@ -40,8 +40,8 @@ class ServiceEntranceAspect {
 
     //TODO 设置 ServiceContext 貌似不应该被 AOP
     @Before("entrancePointcut()")
-    void mdcAndServiceContext(JoinPoint joinPoint){
-        def serviceContext = extractServiceContent(joinPoint.args[4] as Map<String,String>)
+    void serviceContext(JoinPoint joinPoint) {
+        def serviceContext = extractServiceContent(joinPoint.args[4] as Map<String, String>)
         def callId = serviceContext?.getCallId()
         def thisId = UUID.randomUUID().toString()
         if (callId) {
@@ -51,19 +51,17 @@ class ServiceEntranceAspect {
             callId = new CallId(id: thisId, root: thisId)
             serviceContext.setCallId(callId)
         }
-
-        MDC.put(ServiceContext.CALLID.toString(), objectMapper.writeValueAsString(callId))
         ServiceContextHolder.setServiceContext(serviceContext)
     }
 
     @Before("entrancePointcut()")
-    void requestLog(JoinPoint joinPoint){
+    void requestLog(JoinPoint joinPoint) {
         def args = joinPoint.args
         log.info("JSON facade call: ${args[0]}.${args[1]}.${args[2]} request with params ${args[3]} and headers ${args[4]}");
     }
 
-    @AfterReturning(pointcut="entrancePointcut()",returning = "result")
-    void responseLog(JoinPoint joinPoint, ResponseEntity<HttpJsonResponse> result){
+    @AfterReturning(pointcut = "entrancePointcut()", returning = "result")
+    void responseLog(JoinPoint joinPoint, ResponseEntity<HttpJsonResponse> result) {
         def args = joinPoint.args
         log.info("JSON facade call: ${args[0]}.${args[1]}.${args[2]} response with body ${objectMapper.writeValueAsString(result.getBody())} and headers ${result.getHeaders()}")
     }
